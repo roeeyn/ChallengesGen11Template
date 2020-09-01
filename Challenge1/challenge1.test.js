f3babe6b818ff9b89d5c69f068c7a78dd6ac237b
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-08-31 20:14:35 -0500

    fixed tests

--- Challenge1/challenge1.test.js
+++ Challenge1/challenge1.test.js
@@ -1,7 +1,7 @@
 const { operation } = require("./challenge1.js");
 
-test("TESTING main fun", () => expect(operation(12, 12), "added"));
-test("TESTING main fun", () => expect(operation(100, 76), "subtracted"));
-test("TESTING main fun", () => expect(operation(6, 4), "multiplied"));
-test("TESTING main fun", () => expect(operation(528, 22), "divided"));
-test("TESTING main fun", () => expect(operation(10, 12), null));
+test("TESTING main fun", () => expect(operation(12, 12)).toBe("added"));
+test("TESTING main fun", () => expect(operation(100, 76)).toBe("substracted"));
+test("TESTING main fun", () => expect(operation(6, 4)).toBe("multiplied"));
+test("TESTING main fun", () => expect(operation(528, 22)).toBe("divided"));
+test("TESTING main fun", () => expect(operation(10, 12)).toBe(null));
--- Challenge6/challenge6.test.js
+++ Challenge6/challenge6.test.js
@@ -1,37 +1,34 @@
 const { canPayCost } = require("./challenge6.js");
 
-test("TESTING main fun", () => expecet(canPayCost("BB", "1B")).toBe(true));
-test("TESTING main fun", () => expecet(canPayCost("BR", "1B")).toBe(true));
-test("TESTING main fun", () => expecet(canPayCost("UBC", "1B")).toBe(true));
-test("TESTING main fun", () => expecet(canPayCost("UR", "1B")).toBe(false));
-test("TESTING main fun", () => expecet(canPayCost("B", "1B")).toBe(false));
-test("TESTING main fun", () => expecet(canPayCost("U", "U")).toBe(true));
-test("TESTING main fun", () => expecet(canPayCost("WU", "U")).toBe(true));
-test("TESTING main fun", () => expecet(canPayCost("C", "U")).toBe(false));
-test("TESTING main fun", () => expecet(canPayCost("RGG", "RGG")).toBe(true));
-test("TESTING main fun", () => expecet(canPayCost("URGG", "RGG")).toBe(true));
-test("TESTING main fun", () => expecet(canPayCost("RRG", "RGG")).toBe(false));
-test("TESTING main fun", () => expecet(canPayCost("WWGG", "RGG")).toBe(false));
-test("TESTING main fun", () => expecet(canPayCost("WWRCC", "2WWC")).toBe(true));
+test("TESTING main fun", () => expect(canPayCost("BB", "1B")).toBe(true));
+test("TESTING main fun", () => expect(canPayCost("BR", "1B")).toBe(true));
+test("TESTING main fun", () => expect(canPayCost("UBC", "1B")).toBe(true));
+test("TESTING main fun", () => expect(canPayCost("UR", "1B")).toBe(false));
+test("TESTING main fun", () => expect(canPayCost("B", "1B")).toBe(false));
+test("TESTING main fun", () => expect(canPayCost("U", "U")).toBe(true));
+test("TESTING main fun", () => expect(canPayCost("WU", "U")).toBe(true));
+test("TESTING main fun", () => expect(canPayCost("C", "U")).toBe(false));
+test("TESTING main fun", () => expect(canPayCost("RGG", "RGG")).toBe(true));
+test("TESTING main fun", () => expect(canPayCost("URGG", "RGG")).toBe(true));
+test("TESTING main fun", () => expect(canPayCost("RRG", "RGG")).toBe(false));
+test("TESTING main fun", () => expect(canPayCost("WWGG", "RGG")).toBe(false));
+test("TESTING main fun", () => expect(canPayCost("WWRCC", "2WWC")).toBe(true));
+test("TESTING main fun", () => expect(canPayCost("WWURGC", "2WWC")).toBe(true));
+test("TESTING main fun", () => expect(canPayCost("WWCC", "2WWC")).toBe(false));
+test("TESTING main fun", () => expect(canPayCost("WRCCC", "2WWC")).toBe(false));
+test("TESTING main fun", () => expect(canPayCost("WWRRR", "2WWC")).toBe(false));
 test("TESTING main fun", () =>
-  expecet(canPayCost("WWURGC", "2WWC")).toBe(true));
-test("TESTING main fun", () => expecet(canPayCost("WWCC", "2WWC")).toBe(false));
+  expect(canPayCost("WUBRGC", "WUBRG")).toBe(true));
+test("TESTING main fun", () => expect(canPayCost("BBBB", "4")).toBe(true));
+test("TESTING main fun", () => expect(canPayCost("BBCC", "3")).toBe(true));
+test("TESTING main fun", () => expect(canPayCost("BBCC", "5")).toBe(false));
 test("TESTING main fun", () =>
-  expecet(canPayCost("WRCCC", "2WWC")).toBe(false));
+  expect(canPayCost("WWWWUUUUBBBBRR", "12RR")).toBe(true));
 test("TESTING main fun", () =>
-  expecet(canPayCost("WWRRR", "2WWC")).toBe(false));
+  expect(canPayCost("WWWWUUUUBBBBRR", "11RR")).toBe(true));
 test("TESTING main fun", () =>
-  expecet(canPayCost("WUBRGC", "WUBRG")).toBe(true));
-test("TESTING main fun", () => expecet(canPayCost("BBBB", "4")).toBe(true));
-test("TESTING main fun", () => expecet(canPayCost("BBCC", "3")).toBe(true));
-test("TESTING main fun", () => expecet(canPayCost("BBCC", "5")).toBe(false));
+  expect(canPayCost("WWWWUUUUBBBBRR", "13RR")).toBe(false));
 test("TESTING main fun", () =>
-  expecet(canPayCost("WWWWUUUUBBBBRR", "12RR")).toBe(true));
+  expect(canPayCost("WWWWUUUUBBBBRGG", "13RR")).toBe(false));
 test("TESTING main fun", () =>
-  expecet(canPayCost("WWWWUUUUBBBBRR", "11RR")).toBe(true));
-test("TESTING main fun", () =>
-  expecet(canPayCost("WWWWUUUUBBBBRR", "13RR")).toBe(false));
-test("TESTING main fun", () =>
-  expecet(canPayCost("WWWWUUUUBBBBRGG", "13RR")).toBe(false));
-test("TESTING main fun", () =>
-  expecet(canPayCost("WWWWUUUUBBBBRRRRGGGG", "15")).toBe(true));
+  expect(canPayCost("WWWWUUUUBBBBRRRRGGGG", "15")).toBe(true));
